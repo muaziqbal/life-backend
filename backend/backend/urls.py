f19fe1f0c365909ffe08a4855b61a1b0327f1826
muaziqbal <sheikhmuaz111@gmail.com>
2025-07-08 02:39:52 +0500

    Addedd authentication

---
+++ backend/backend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # refresh
+    path('api/auth/', include('auth_service.urls')),  # include your app’s routes
+]
